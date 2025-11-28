5880bfe38695658bc748a439333329b3e7655dd2
pawan507 <pawanBhattarai507@gmail.com>
2025-11-28 01:25:30 +0100

    dark mode option applied on all pages

--- javaScript/changeMode.js
+++ javaScript/changeMode.js
@@ -4,6 +4,41 @@
  */
 
 const modeBtn=document.getElementById("mode-btn");
+
+/*
+//this event listener is enough to change theme but won't remain if reloaded.
+//also we need to add this script to all available pages to maintain the consistent theme
 modeBtn.addEventListener("click",() =>{
 	document.body.classList.toggle("dark-mode");
 });
+*/
+
+if(modeBtn!=null){
+    //change theme on click on button "change Mode"
+    modeBtn.addEventListener("click",() =>{
+        document.body.classList.toggle("dark-mode");
+
+        //save the current theme to local computer
+        if(document.body.classList.contains("dark-mode")){
+            localStorage.setItem("theme","dark-theme");
+        }
+
+        else{
+        localStorage.setItem("theme","light-theme");
+        }
+    });
+
+    // apply saved theme when page is loaded again
+    window.addEventListener("load", () => {
+        if(localStorage.getItem("theme") == "dark-theme") {
+            document.body.classList.add("dark-mode");
+        }
+    });
+
+}
+
+else{
+    if(localStorage.getItem("theme") == "dark-theme") {
+            document.body.classList.add("dark-mode");
+        }
+}
